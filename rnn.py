dcbd2ab9851f06724483d2c84defd6f1284e7d6d
raghav-ai <67466406+raghav-ai@users.noreply.github.com>
2024-05-11 22:36:36 -0400

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,10 +1,3 @@
-# Recurrent Neural Network
-
-
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -41,37 +34,21 @@ from keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
-
-# Adding the first LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
-
-# Adding a second LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
-# Adding a third LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
-# Adding a fourth LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
-
-# Adding the output layer
 regressor.add(Dense(units = 1))
-
-# Compiling the RNN
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
-# Fitting the RNN to the Training set
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 
 
 # Part 3 - Making the predictions and visualising the results
-
-# Getting the real stock price of 2017
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
@@ -88,7 +65,6 @@ X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
-# Visualising the results
 plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
 plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
 plt.title('Google Stock Price Prediction')
